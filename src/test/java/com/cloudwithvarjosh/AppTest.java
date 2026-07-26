9a7f3fed9200ba2ab06bfb2fb7dce282e08e7c1e
anupZe <anupwork26@gmail.com>
2026-07-26 20:30:08 +0000

    move Dockerfile to root

--- src/test/java/com/cloudwithvarjosh/AppTest.java
+++ src/test/java/com/cloudwithvarjosh/AppTest.java
@@ -9,7 +9,7 @@ public class AppTest {
     void testBrandHtmlContainsBrand() {
         String h = App.brandHtml();
         assertNotNull(h);
-        assertTrue(h.contains("Cloud With VarJosh"));
+        assertTrue(h.contains("Hello bro"));
     }
 
     @Test
